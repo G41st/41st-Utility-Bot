48c33766f4a664738e1df6066c263ceca32ff98e
Nick T <nicholastroiano123@gmail.com>
2022-01-24 12:38:19 -0500

    started overide dev, fonna get it perfected

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import overides
 import discord
 from discord.ext import commands
 from discord.ext.commands import bot
@@ -34,6 +35,9 @@ async def on_message(message):
         return
 
     await message.channel.send("hola")
+
+bot.command(name='overide')
+async def overide(command):
 
 
 bot.run(TOKEN)
---
+++ overides.py
@@ -0,0 +1,9 @@
+# bot.py
+import os
+import random
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+from dotenv import load_dotenv
+
+bot.command
