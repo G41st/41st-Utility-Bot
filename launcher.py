fe99c31e589dd6aea5ee168ffaafa03165b621ae
Nick T <nicholastroiano123@gmail.com>
2022-01-25 09:16:22 -0500

    actualy made launcher.py work, it wasnt going to before

--- launcher.py
+++ launcher.py
@@ -9,7 +9,8 @@ t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 
 def bypass():
-    bypass = input("The install verification file failed. do you want to install the program, or do you want to pass? \n type [1] to pass and launch the program, type [2] to reinstall \n")
+    bypass = input("The install verification file failed. do you want to install the program, or do you want to pass? "
+                   "\n type [1] to pass and launch the program, type [2] to reinstall \n")
 
     if bypass == "1":
         bot.main()
@@ -39,8 +40,7 @@ def install_checker():
         for lines in setup_file:
             if readline(lines) == True:
                 print('installed. launching...')
-                welcome()
-                usr_input()
+                bot.main()
                 break
             elif readline(lines) == False:
                 print('not installed. installing...')
@@ -51,22 +51,6 @@ def install_checker():
 
     setup_file.close()
 
-def welcome():
-    account_file = open("Account.txt","r")
-    account_file_read = account_file.readlines()
-    account_name = account_file_read[1]
-    full_name = account_file_read[2:3]
-    first_name = account_file_read[2]
-    last_name = account_file_read[3]
-    email = account_file_read[4]
-
-    print("logging into account",account_name,"...")
-    time.sleep(0.5)
-    buffering()
-    print("----------------------------------------------------")
-    print("Welcome",first_name)
-    print("loaded v0.1")
-    account_file.close()
 
 def buffering():
     print(".")
@@ -75,43 +59,6 @@ def buffering():
     time.sleep(0.5)
     print(".")
     time.sleep(0.5)
-
-def usr_input():
-
-    def cooker_handler():
-        cooker.main()
-
-    def profile_handler():
-        profile_manager.profile_list()
-
-    def settings_handler():
-        settings_manager.user_input()
-
-    def troll_handler():
-        easter_eggs.troll()
-        usr_input()
-
-    def floppa_handler():
-        easter_eggs.floppa()
-        usr_input()
-
-    print("what do you want to do?")
-    question = input(" \n type [1] to cook, \n type [2] to view/edit your profiles, \n type [3] to edit your settings. \n \n $")
-
-    handlers = {
-        '1': cooker_handler,
-        '2': profile_handler,
-        '3': settings_handler,
-        'troll': troll_handler,
-        'floppa': floppa_handler
-    }
-
-    if question in handlers:
-        handlers[question]()
-    else:
-        print("Unknown choice", question)
-
-
 
 
 path = os.getcwd()
