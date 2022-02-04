ee7faec46068ea3e01709c02c2e05382da7240b0
Nick T <nicholastroiano123@gmail.com>
2022-02-03 21:41:27 -0500

    tons of things

--- git_pull.py
+++ git_pull.py
@@ -3,7 +3,7 @@ from git.repo.base import Repo
 import shutil
 import time
 
-def download(filename):
+def download1(filename):
     cwd = os.getcwd()
 
     try:
@@ -22,6 +22,50 @@ def download(filename):
     except FileNotFoundError:
         Repo.clone_from("https://github.com/G41st-Bot/41st-utility-bot", f"{cwd}/41st-bot")
 
+        os.remove(filename)
+
+        shutil.copy(f"{cwd}/41st-bot/{filename}", f"{cwd}")
+
+        os.remove(f"41st-bot/{filename}")
+
+        time.sleep(10)
+
+
+def download2(filename):
+    cwd = os.getcwd()
+
+    try:
+        os.remove(filename)
+
+        shutil.copy(f"{cwd}/41st-bot/{filename}", f"{cwd}")
+
+        os.remove(f"41st-bot/{filename}")
+
+        time.sleep(10)
+
+    except FileNotFoundError:
+        os.remove(filename)
+
+        shutil.copy(f"{cwd}/41st-bot/{filename}", f"{cwd}")
+
+        os.remove(f"41st-bot/{filename}")
+
+        time.sleep(10)
+
+
+def download2(filename):
+    cwd = os.getcwd()
+
+    try:
+        os.remove(filename)
+
+        shutil.copy(f"{cwd}/41st-bot/{filename}", f"{cwd}")
+
+        os.remove(f"41st-bot/{filename}")
+
+        time.sleep(10)
+
+    except FileNotFoundError:
         os.remove(filename)
 
         shutil.copy(f"{cwd}/41st-bot/{filename}", f"{cwd}")
