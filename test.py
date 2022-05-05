921376b4ea37b633958e3d360baa240c690fd926
CT-42210 <nicholastroiano123@gmail.com>
2022-05-05 13:23:08 -0400

    suggestion command basics

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import datetime
 import discord
 from discord.ext.commands import Bot
 from discord import Intents
+from discord.ui import Select, View
 import assets
 import register_command
 import role_analyze
@@ -64,9 +65,58 @@ async def on_ready():
     await bot_command_channel.send(message)
 
 
-@bot.command(name='suggestion')
+@bot.command(name='test')
+async def suggestion_command(ctx):
+    button1 = discord.ui.Button(label="hello", style=discord.ButtonStyle.green)
+    button2 = discord.ui.Button(label="goodbye", style=discord.ButtonStyle.danger)
+
+    select = Select(
+        min_values=2,
+        max_values=3,
+        placeholder="Enter Tags",
+        options=[
+            discord.SelectOption(
+                label="Army",
+                description="Anything relating to Regiments, or ground game modes."
+            ),
+            discord.SelectOption(
+                label="Navy",
+                description="Anything relating to Wings or Space Gamemodes."
+            ),
+            discord.SelectOption(
+                label="Logistics",
+                description="Logistics"
+            )
+        ],
+        row=2
+    )
+
+    async def select_callback(interaction):
+        await interaction.response.send_message(view=view2)
+
+    async def button1_callback(interaction):
+        await interaction.response.send_message(f"You chose: {select.values}, {button1.label}")
+
+    async def button2_callback(interaction):
+        await interaction.response.send_message(f"You chose: {select.values}, {button2.label}")
+
+    view1 = View()
+    view2 = View()
+
+    view1.add_item(select)
+    view2.add_item(button1)
+    view2.add_item(button2)
+    select.callback = select_callback
+    button1.callback = button1_callback
+    button2.callback = button2_callback
+
+
+    await ctx.send("hello", view=view1)
 
 
 def main():
     while True:
         bot.run(LAUNCH)
+
+
+main()
