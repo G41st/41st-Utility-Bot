3c74af0caf9b128a9f5801af92a86a63202d88d0
Nick T <nicholastroiano123@gmail.com>
2022-05-07 21:03:51 -0400

    maybe it be working

--- test2.py
+++ test2.py
@@ -2,31 +2,21 @@ from pathlib import Path
 import linecache
 
 directory = Path.cwd()
-merit_path = "merit.txt".format(directory)
-demerit_path = "demerit.txt".format(directory)
+merit_path = "merit.txt"
+demerit_path = "demerit.txt"
+
+
+def merit_reader(discord_id):
+    d_id = str(discord_id)
+
+    with open(merit_path, encoding='utf8') as file:
+        for i, line in enumerate(file):
+            if line.strip() == d_id:
+                merit_total = (linecache.getline(merit_path, i + 2)).strip()
+                print(i)
+    return merit_total
+
 
 print(merit_path)
 
 
-def add_credits(discord_id, new_credit_value):
-    d_id = str(discord_id)
-
-    with open(merit_path, encoding='utf8') as file:
-        content = file.readlines()
-        for i, line in enumerate(file):
-            if d_id in line.strip():
-                current_merit_total = (linecache.getline(merit_path, i + 2)).strip()
-
-                new_credit_total = current_merit_total + new_credit_value
-
-                content[i + 1] = str(new_credit_total) + "\n"
-
-                with open(merit_path, encoding='utf8') as f:
-                    f.writelines(content)
-
-                print(merit_path)
-                print(current_merit_total)
-                print(content[i + 1])
-
-
-add_credits(583386313466708035, 1)
