3226e0566a81e7f7a4cbfd21e20d1691533a6800
Nick T <nicholastroiano123@gmail.com>
2022-12-16 10:43:45 -0500

    IT FUCKING WORKS

--- start.sh
+++ start.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-restart_count=0
+# Set a counter to track the number of times the Python script has exited
+counter=0
 
+# Set a flag to track whether the Python script has exited more than once within a minute
+flag=false
+
+# Set the timeout to one minute (60 seconds)
+timeout=60
+restart=5
+
+# Start a loop to run the Python script
 while true; do
-  python pc_start.py
+  # Run the Python script using the `timeout` command
+  # If the Python script takes longer than $timeout seconds to run, it will be killed
+  timeout $timeout python pc_start.py
 
-  restart_count=$((restart_count + 1))
+  # Increment the counter
+  counter=$((counter+1))
 
-  if [ "$restart_count" -ge 2 ] && [ $(($(date +%s) - $(date +%s -r my_file.py))) -lt 60 ]; then
-
+  # If the Python script has exited more than once within a minute, set the flag to true
+  if [ $counter -gt 1 ]; then
+    flag=true
     break
-
-  else
-    restart_count=0
-
   fi
+
+  # Sleep for one minute before running the Python script again
+  sleep $restart
 done
+
+# If the flag is true, exit the Bash script
+if [ "$flag" = true ]; then
+  exit
+fi
