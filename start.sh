c7dbc72cbf38be259360d9ef182ffa77accf1bf0
Nick T <nicholastroiano123@gmail.com>
2022-12-16 19:35:22 -0500

    v3.2.0 bruh medal moment

--- start.sh
+++ start.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-# Set a counter to track the number of times the Python script has exited
 counter=0
 
-# Set a flag to track whether the Python script has exited more than once within a minute
 flag=false
 
-# Set the timeout to one minute (60 seconds)
 timeout=60
 restart=5
 
-# Start a loop to run the Python script
 while true; do
-  # Run the Python script using the `timeout` command
-  # If the Python script takes longer than $timeout seconds to run, it will be killed
-  timeout $timeout python pc_start.py
+  python3 pc_start.py
 
-  # Increment the counter
   counter=$((counter+1))
 
-  # If the Python script has exited more than once within a minute, set the flag to true
   if [ $counter -gt 1 ]; then
     flag=true
     break
   fi
 
-  # Sleep for one minute before running the Python script again
   sleep $restart
 done
 
-# If the flag is true, exit the Bash script
 if [ "$flag" = true ]; then
   exit
 fi
